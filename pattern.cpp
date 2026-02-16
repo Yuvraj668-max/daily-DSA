647cfe765b5dc0d31bcb0808f6d4c45d67dc7eb2
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-16 23:07:51 +0530

    pattern 4

--- pattern.cpp
+++ pattern.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-for(int i=1;i<=4;i++)
+for(int i=0;i<4;i++)
 {
-    for(int j=1;j<=4;j++)
+    char ch ='A';
+    for(int j=0;j<4;j++)
     {
-        cout<<j<<" ";
+        cout<<ch<<" ";
+        ch=ch+1;
     }
     cout<<endl;
 }
