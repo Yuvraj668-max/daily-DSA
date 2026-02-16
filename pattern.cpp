50df3b6011827c80cc7495b07ba1f69326e5d0cf
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-17 00:30:27 +0530

    pattern 11

--- pattern.cpp
+++ pattern.cpp
@@ -4,14 +4,16 @@ int main(){
   
 int n=4;
 
+char ch='A';
+
 for(int i=0;i<n;i++)
 {
 
     
     for(int j=i+1;j>0;j--)
     {
-        cout<<j<<" ";
-        
+        cout<<ch<<" ";
+        ch=ch+1;
         
     }
     cout<<endl;
