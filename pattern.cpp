d7f2f2752b30c11748117cdceeedcc4ece2a9dea
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-17 00:42:16 +0530

    pattern 13

--- pattern.cpp
+++ pattern.cpp
@@ -8,16 +8,17 @@ int n=4;
 
 for(int i=0;i<n;i++)
 {
-
-    
-    for(int j=i+1;j>0;j--)
+    for (int j=0;j<n-i;j++)
     {
         cout<<" ";
-        
-        
     }
-    for (int k=n;k>i;k--){
-        cout<<i+1;
+    for (int k=0;k<i+1;k++)
+    {
+        cout<<k+1;
+    }
+    for (int l=i;l>0;l--)
+    {
+        cout<<l;
     }
     cout<<endl;
     
