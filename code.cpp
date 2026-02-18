39644a315806c106a1cd1eaf855b7aa4641e9ef4
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-18 23:54:46 +0530

    binary to decimal

--- code.cpp
+++ code.cpp
@@ -6,19 +6,20 @@ using namespace std;
 int main()
 {
  
+int binary;
 int pow=1,ans=0;
-int decNum;
-cout<<"enter number";
-cin>>decNum;
-while (decNum>0){
-    int rem=decNum%2;
-    decNum=decNum/2;
-    ans = ans + (rem*pow);
-    pow =pow*10;
+cout<<"enter binary numaber"<<endl;
+cin>>binary;
+while(binary>0){
+    int rem=binary%10;
+    ans = ans+ (rem*pow);
+    binary=binary/10;
+    pow=pow*2;
 
-
+    
 }
-cout<<"binary is "<<ans;
+
+cout<<"decimal num is "<<ans;
 }
 
 
