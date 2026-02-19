8d5880d041943e523e48ead9dd78cf905635961a
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-19 13:05:33 +0530

    power 0f 2

--- code.cpp
+++ code.cpp
@@ -1,41 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int countDigits(int n){
-    int digit=0;
-    while(n>0){
-        digit++;
-        n=n/10;
+bool isPowerOfTwo(int n,int current=1)
+{
+    
+    if(current==n){
+        return true;
+    }
+    if(current>n){
+        return false;
+    }
 
-    }
-    return digit;
+    return isPowerOfTwo(n,current<<1);
 }
-int reverseNum(int n,int digit){
-    int newNum=0;
-    int pow=1;
-    while(digit>1){
-        pow=pow*10;
-        digit--;
-    }
-    while(n>0){
-        int rem=n%10;
-        newNum= newNum + (rem*pow);
-        n=n/10;
-        pow=pow/10;
-    }
-    return newNum;
-}
-
 int main()
 {
  int n;
  cout<<"enter a number :";
  cin>>n;
- int digit=countDigits(n);
- int reverseN= reverseNum(n,digit);
- cout<<reverseN;
- 
-
+ if(isPowerOfTwo(n)){
+    cout<<"number is a power of 2";
+ }
+ else{
+    cout<<"number is not a power of 2";
+ }
 }
 
 
