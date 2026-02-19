e365d03b3d588810b141d045f3a3b640a44c19a5
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-19 12:46:55 +0530

    reverse a number

--- code.cpp
+++ code.cpp
@@ -1,25 +1,41 @@
 #include <iostream>
 using namespace std;
 
+int countDigits(int n){
+    int digit=0;
+    while(n>0){
+        digit++;
+        n=n/10;
 
+    }
+    return digit;
+}
+int reverseNum(int n,int digit){
+    int newNum=0;
+    int pow=1;
+    while(digit>1){
+        pow=pow*10;
+        digit--;
+    }
+    while(n>0){
+        int rem=n%10;
+        newNum= newNum + (rem*pow);
+        n=n/10;
+        pow=pow/10;
+    }
+    return newNum;
+}
 
 int main()
 {
+ int n;
+ cout<<"enter a number :";
+ cin>>n;
+ int digit=countDigits(n);
+ int reverseN= reverseNum(n,digit);
+ cout<<reverseN;
  
-int binary;
-int pow=1,ans=0;
-cout<<"enter binary numaber"<<endl;
-cin>>binary;
-while(binary>0){
-    int rem=binary%10;
-    ans = ans+ (rem*pow);
-    binary=binary/10;
-    pow=pow*2;
 
-    
-}
-
-cout<<"decimal num is "<<ans;
 }
 
 
