b4a855e38148c2908b0806d66aea917229cc96e0
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-24 19:36:15 +0530

    bruteforce method to print all the subarray in O(n^2)

--- code.cpp
+++ code.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
-#include <vector>
+// #include <vector>
+#include <climits> 
 using namespace std;
 
 int main(){
     int arr[]={1,2,3,4,5};
     int n=5;
+    int maxSum=INT_MIN;
     for (int st=0;st<n;st++)
     {
+        int currSum=0;
         for (int end=st;end<n;end++)
         {
-            for(int i=st;i<=end;i++)
-            {
-                cout<<arr[i]<<" ";
-            }
-            cout<<endl;
+            currSum=currSum+arr[end];
+             maxSum=max(currSum, maxSum);
+           
         }
-        // cout<<endl;
+     
     }
+    cout<<"max sub array sum "<<maxSum;
+    return 0;
 }
