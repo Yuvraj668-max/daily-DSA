30da2bf267c447e40b61b15d136f28a5be730027
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-25 17:55:12 +0530

    maojirty element optimised method

--- code.cpp
+++ code.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include<vector>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 int majorityElement(vector<int> &nums)
 {
-    int n= nums.size()-1;
-    for(int val:nums)
-    {
-        int freq=0;
-        for(int vlu:nums)
+    int n= nums.size();
+    sort(nums.begin(),nums.end());
+     int freq=1;
+     int ans=nums[0];
+     for(int i=1;i<n;i++)
+     {
+        if (nums[i]==nums[i-1])
         {
-            if(val==vlu)
-            {
-                freq++;
+            freq++;
+        }
+        else 
+        {
+            freq=1;
+            ans =   nums[i];
+        }
 
-            }
-        }
-        if(freq>n/2)
+        if (freq>n/2)
         {
-            return val;
+            return nums[i];
         }
-    }
+     }
 }
 int main()
 {
-    vector<int> nums={1,2,2,1,1};
+    vector<int> nums={1,2,2,1,1,2,2,2,2,2};
     int ans=majorityElement(nums);
     cout<<ans;
 
