21eb6be8e158d6a4dcb05449476be8ed06b479ca
Yuvraj668-max <yuvrajkumarrkmv@gmail.com>
2026-02-25 17:33:20 +0530

    pair sum optimal solution

--- code.cpp
+++ code.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
-// #include <vector>
+#include <vector>
 #include <climits> 
 using namespace std;
 
-int main(){
-    int arr[]={1,2,3,4,5};
-    int n=5;
-    int maxSum=INT_MIN;
-    for (int st=0;st<n;st++)
+vector<int> pairSum(vector<int> &nums,int target)
+{
+    int st=0;
+    int end=nums.size()-1;
+    int ps;
+    vector<int> ans;
+    while(st<end)
     {
-        int currSum=0;
-        for (int end=st;end<n;end++)
+        ps=nums[st]+nums[end];
+        if(ps < target)
         {
-            currSum=currSum+arr[end];
-             maxSum=max(currSum, maxSum);
-           
+            st++;
         }
-     
+        else if(ps > target)
+        {
+            end--;
+        }
+        else{
+            ans.push_back(st);
+            ans.push_back(end);
+            return ans;
+
+        }
+
     }
-    cout<<"max sub array sum "<<maxSum;
+    return ans;
+}
+
+int main(){
+  vector<int>nums ={1,2,3,4,5};
+  int target =7;
+  vector<int> ans=pairSum(nums,target);
+
+  cout<<ans[0]<<", "<<ans[1]<<endl;
+
     return 0;
 }
